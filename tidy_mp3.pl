454f2ec44b881f2997dc56f40c73321484dbf271
Jozef Reisinger <jozef.reisinger@gmail.com>
2012-08-08 16:24:21 +0200

    add some tag checking/customization

--- tidy_mp3.pl
+++ tidy_mp3.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+
 # Create folder hierarchy based on MP3 tags.
 # Based on: http://www.perlmonks.org/?node_id=985820
 # To get mp3 files from directories
@@ -8,21 +9,33 @@ use warnings;
 use MP3::Tag;
 use File::Copy::Recursive qw(fmove);
 
+my $Debug = 1;    # 1 - don't move files just print the new path
+
 foreach my $file (<*.mp3>) {
     my $mp3 = MP3::Tag->new($file);
-    my ( $song, $track, $artist, $album ) = $mp3->autoinfo();
+    my ( $title, $track, $artist, $album ) = ( $mp3->autoinfo() )[ 0, 1, 2, 3 ];
     $mp3->close();
-    $song   = $song   eq '' ? 'uknown_song'   : $song;
-    $track  = $track  eq '' ? 'uknown_track'  : $track;
+
+    # Check/customize tags
+    $track = $track =~ /^([0-9]+)/ ? sprintf "%02d", $1 : undef;
+    $title  = $title  eq '' ? 'uknown_title'  : $title;
     $artist = $artist eq '' ? 'uknown_artist' : $artist;
     $album  = $album  eq '' ? 'uknown_album'  : $album;
-
-    # Debug.
-    #print "### $file ###\n";
-    #for ( $song, $track, $artist, $album ) {
-    #    print "===> |$_|\n";
-    #}
-
     s/[\\\/:*?"<>|]//g for $artist, $album;
-    fmove( $file, "$artist/$album/$file" ) or die "Can't fmove '$file': $!";
+
+    my $path;
+    if ( defined $track ) {    # track number tag present
+        $path = "$artist/$album/$track $title.mp3";
+    }
+    else {                     # no track number tag
+        $path = "$artist/$album/$title.mp3";
+    }
+
+    if ($Debug) {
+        print "$path\n";
+    }
+    else {
+        fmove( $file, $path )
+          or warn "Can't fmove '$file': $!";
+    }
 }
