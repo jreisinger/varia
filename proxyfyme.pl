a97e52c91696078cff32e734c31b55b471b91511
Jozef Reisinger <jozef.reisinger@gmail.com>
2012-08-28 10:47:45 +0200

    fix if we don't have curl

--- proxyfyme.pl
+++ proxyfyme.pl
@@ -9,20 +9,22 @@ my $wpad_url = 'http://wpad/wpad.dat';
 my %conf;
 
 # Automatic proxy configuration.
-my $html     = `curl -s $wpad_url`;
-$conf{server} = $html =~ /Node\("?([0-9\.]+)"?/      ? $1 : "";
-$conf{port}   = $html =~ /HttpPort="?([0-9]{1,5})"?/ ? $1 : "";
-$conf{user}   = $ENV{USER} ? $ENV{USER} : "";
+my $html;
+if ( ! system "which curl > /dev/null" ) {  # do we have curl installed?
+  $html = `curl -s $wpad_url`;
+} else {
+  $html = "";
+}
+$conf{server} = $html =~ /Node\("?([0-9\.]+)"?/ ? $1 : "";
+$conf{port} = $html =~ /HttpPort="?([0-9]{1,5})"?/ ? $1 : "8080";
+$conf{user} = $ENV{USER} ? $ENV{USER} : "";
 
-# Check automatic proxy configuration succeeded.
-my $count = grep $conf{$_} =~ /\S+/, keys %conf;
-if ( $count != 3 ) {    # not all parameters set
-    for (qw( user server port )) {
-        print "Enter proxy $_ [defaults to: '$conf{$_}']> ";
-        chomp( my $input = <STDIN> );
-        next if $input =~ /^\s*$/;    # keep the previous value
-        $conf{$_} = $input;
-    }
+# Confirm values with the user
+for (qw( user server port )) {
+    print "Enter proxy $_ [defaults to: '$conf{$_}']> ";
+    chomp( my $input = <STDIN> );
+    next if $input =~ /^\s*$/; # keep the previous value
+    $conf{$_} = $input;
 }
 
 # Get proxy password from user
@@ -33,6 +35,6 @@ system "stty echo";
 print "\n";
 
 # Set proxy and run command(s)
-$ENV{http_proxy}  = "http://$conf{user}:$pass\@$conf{server}:$conf{port}";
+$ENV{http_proxy} = "http://$conf{user}:$pass\@$conf{server}:$conf{port}";
 $ENV{https_proxy} = "http://$conf{user}:$pass\@$conf{server}:$conf{port}";
 exec "@ARGV";
