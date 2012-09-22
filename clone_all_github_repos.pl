cbc844980f58a2cc62d21b64c633aa82e2dda86e
Jozef Reisinger <jozef.reisinger@gmail.com>
2012-09-22 04:22:31 +0200

    add script to clone all GitHub repos

---
+++ clone_all_github_repos.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Net::GitHub;
+use Data::Dumper;
+
+print "GitHub login> ";
+chomp(my $login = <STDIN>);
+system "stty -echo";
+print "GitHub password> ";
+chomp(my $pass = <STDIN> );
+system "stty echo";
+print "\n";
+
+# default to v3
+my $github = Net::GitHub->new(  # Net::GitHub::V3
+    login => $login, pass => $pass
+);
+
+my @repos = $github->repos->list;
+
+#print Dumper \@repos;
+
+for my $repo (@repos) {
+    system "git clone $repo->{ssh_url}";
+}
